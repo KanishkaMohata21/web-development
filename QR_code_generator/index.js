24dac5eb767d3d1d9097e1c5eb58c91e0c247bb1
KanishkaMohata21 <132136054+KanishkaMohata21@users.noreply.github.com>
2023-10-01 13:23:55 +0530

    Add files via upload

---
+++ QR_code_generator/index.js
@@ -0,0 +1,20 @@
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
+
+inquirer
+.prompt([{
+    message:"Type in your URL",
+    name:"URL"
+}])
+
+.then((answer)=>{
+    const url=answer.URL;
+    var qr_svg=qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+    fs.writeFile("url.txt",url,(err)=>{
+        if (err) throw err;
+        console.log("THE FILE HAS BEEN SAVED");
+    });
+})
+
