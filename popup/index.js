c54d2cbe30dce01ae225f7954ef70c6f82525434
KanishkaMohata21 <132136054+KanishkaMohata21@users.noreply.github.com>
2023-12-31 23:43:10 +0530

    Add files via upload

---
+++ popup/index.js
@@ -0,0 +1,9 @@
+let popup = document.querySelector(".popup");
+
+function display_popup() {
+    popup.classList.add("open_popup");
+}
+
+function hide_popup() {
+    popup.classList.remove("open_popup");
+}
